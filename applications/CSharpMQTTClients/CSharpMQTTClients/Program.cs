68f8558cf63a9af8823d98e584e004e505efffc0
David Tarnow <david.tarnow@outlook.de>
2018-05-20 22:40:01 +0200

    Added M2MQTT Library

--- applications/CSharpMQTTClients/CSharpMQTTClients/Program.cs
+++ applications/CSharpMQTTClients/CSharpMQTTClients/Program.cs
@@ -1,15 +1,64 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using uPLibrary.Networking.M2Mqtt.Messages;
 
-namespace CSharpMQTTClients
+namespace uPLibrary.Networking.M2Mqtt
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static string MQTT_BROKER_ADDRESS = "192.168.178.37";
+
+        private static void Main(string[] args)
         {
+            if (args.Length > 0) // publisher
+            {
+                Publisher();
+            }
+            else
+            {
+                Subscriber();
+            }
         }
+
+        private static void Publisher()
+        {
+            Console.WriteLine("Started Publisher");
+
+            // create client instance 
+            var client = new MqttClient(MQTT_BROKER_ADDRESS); 
+ 
+            var clientId = Guid.NewGuid().ToString(); 
+            client.Connect(clientId);
+
+            byte[] sendBytes =
+            {
+                0x00, 0x01
+            };
+ 
+            // publish a message on "/home/temperature" topic with QoS 2 
+            client.Publish("/home/temperature", sendBytes, MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, false); 
+        }
+
+        private static void Subscriber()
+        {
+            Console.WriteLine("Started Subscriber");
+
+            // create client instance 
+            var client = new MqttClient(MQTT_BROKER_ADDRESS); 
+ 
+            // register to message received 
+            client.MqttMsgPublishReceived += client_MqttMsgPublishReceived; 
+ 
+            var clientId = Guid.NewGuid().ToString(); 
+            client.Connect(clientId); 
+ 
+            // subscribe to the topic "/home/temperature" with QoS 2 
+            client.Subscribe(new[] { "/home/temperature" }, new[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
+        }
+
+        private static void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e) 
+        { 
+            // handle message received 
+            Console.WriteLine("Received message");
+        } 
     }
 }
